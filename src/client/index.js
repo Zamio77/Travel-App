71450e62db09d61836ad6a8732ad7ae883e92d95
Doug Lamar <38842486+Zamio77@users.noreply.github.com>
2020-06-27 21:19:24 -0400

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,12 @@
 import './styles/style.scss'
+import { performAction } from './js/app.js'
+import { getWeatherData } from './js/app.js'
+import { postData } from './js/app.js'
+import { updateUI } from './js/app.js'
 
-
-export {}
+export {
+    performAction,
+    getWeatherData,
+    postData,
+    updateUI
+}
