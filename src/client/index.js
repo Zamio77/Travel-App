2ce09204ed9655ae0f36d2f8039048c2ab632000
Doug Lamar <38842486+Zamio77@users.noreply.github.com>
2020-06-29 21:53:29 -0400

    Test Functions
    Testing basic functionality. Then will begin building out web design.

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,8 @@
 import './styles/style.scss'
-import { performAction } from './js/app.js'
-import { getWeatherData } from './js/app.js'
-import { postData } from './js/app.js'
-import { updateUI } from './js/app.js'
+import { getName } from './js/app.js'
+
+document.getElementById('form-submit').addEventListener('click', getName);
 
 export {
-    performAction,
-    getWeatherData,
-    postData,
-    updateUI
+    getName
 }
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -5,3 +5,7 @@ let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
+export const getName = () => {
+  const name = document.getElementById('destination').value
+  document.getElementById("url").innerHTML = `Hello ${name}`;
+}
